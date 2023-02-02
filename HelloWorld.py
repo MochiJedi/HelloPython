986801ab77a35dc459f56ac7a122e3c13af228bb
MochiJedi <111827659+MochiJedi@users.noreply.github.com>
2023-02-02 08:41:01 -0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,8 +4,13 @@ choice = 0
 while int(choice) != 10:
   print("""   Another "Hello World"   """)
   print("1.Hello World!")
+  print("2.Simple Calculator")
   print("10.Quit")
   choice = input("Please enter a choice:")
   if int(choice) == 1:
     print("Hello World!")
+  elif int(choice) == 2:
+    x = input("Please enter a number:")
+    y = input("Please enter another number:")
+    print("Here's what you get when you combine them(hopefully):" + str(int(x) + int(y)))
     
